c5ae648abf40d2be6ddf2eb5c6109ec2acaf6e48
Vince Fleming <46109871+vince-weka@users.noreply.github.com>
2023-04-13 10:23:43 -0400

    Add optional key= parm

--- streamlit_authenticator/authenticate.py
+++ streamlit_authenticator/authenticate.py
@@ -207,7 +207,7 @@ class Authenticate:
         if location not in ['main', 'sidebar']:
             raise ValueError("Location must be one of 'main' or 'sidebar'")
         if location == 'main':
-            if st.button(button_name):
+            if st.button(button_name, key):
                 self.cookie_manager.delete(self.cookie_name)
                 st.session_state['logout'] = True
                 st.session_state['name'] = None
