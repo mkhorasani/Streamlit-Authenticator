25fc3c552331d617d87c2c80a113e695eb2f5ef7
Dragos Velicanu <dragosvelicanu@gmail.com>
2023-04-29 06:53:49 -0400

    update docstrings

--- streamlit_authenticator/validator.py
+++ streamlit_authenticator/validator.py
@@ -3,19 +3,46 @@ import re
 class Validator:
     def validate_username(self, username: str) -> bool:
         """
-        Returns True if the username is 1-20 chars and only alphanumerics and - _
+        Checks the validity of the entered username.
+        Parameters
+        ----------
+        username: str
+            The usernmame to be validated.
+
+        Returns
+        -------
+        bool
+            Validity of entered username.
         """
         pattern = r"^[a-zA-Z0-9_-]{1,20}$"
         return bool(re.match(pattern, username))
 
     def validate_name(self, name: str) -> bool:
         """
-        Returns true if the name is between 1-100 chars
+        Checks the validity of the entered name.
+        Parameters
+        ----------
+        name: str
+            The name to be validated.
+
+        Returns
+        -------
+        bool
+            Validity of entered name.
         """
         return 1 < len(name) < 100
 
     def validate_email(self, email: str) -> bool:
         """
-        Returns true if the email is between 3-320 chars and has an @ symbol
+        Checks the validity of the entered email.
+        Parameters
+        ----------
+        email: str
+            The email to be validated.
+        Returns
+
+        -------
+        bool
+            Validity of entered email.
         """
         return "@" in email and 2 < len(email) < 320
