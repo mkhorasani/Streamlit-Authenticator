6d60fde16d27a41358bf99e8aa68a7f887821e1f
Dragos Velicanu <dragosvelicanu@gmail.com>
2023-04-29 07:04:15 -0400

    undo delete of setup.py and revert changes

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamlit-authenticator",
+    version="0.2.1",
+    author="Mohammad Khorasani",
+    author_email="khorasani.mohammad@gmail.com",
+    description="A secure authentication module to validate user credentials in a Streamlit application.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mkhorasani/Streamlit-Authenticator",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=['Python', 'Streamlit', 'Authentication', 'Components'],
+    python_requires=">=3.6",
+    install_requires=[
+        "PyJWT >=2.3.0",
+        "bcrypt >= 3.1.7",
+        "PyYAML >= 5.3.1",
+        "streamlit >= 0.86",
+        "extra-streamlit-components >= 0.1.55"
+    ],
+)
