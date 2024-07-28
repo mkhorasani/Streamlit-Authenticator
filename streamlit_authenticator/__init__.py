ad553fe1d3ea78a1ea1e26d963904cc46c3ec5a1
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2024-07-28 21:11:23 +0300

    Update __init__.py

--- streamlit_authenticator/__init__.py
+++ streamlit_authenticator/__init__.py
@@ -28,7 +28,7 @@ if not _RELEASE:
     with open('../config.yaml', 'r', encoding='utf-8') as file:
         config = yaml.load(file, Loader=SafeLoader)
 
-    # Hashing all plain text passwords once
+    # Pre-hashing all plain text passwords once
     # Hasher.hash_passwords(config['credentials'])
 
     # Creating the authenticator object
