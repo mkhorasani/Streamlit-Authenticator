0366416905e0b99638f4f5ffee3f4bf1bf0e97ba
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2024-08-21 20:20:57 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath("C:/Users/Mohammad Khorasani/Desktop/Streamlit-Authenticator/streamlit_authenticator"))
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'streamlit-authenticator'
 copyright = '2024, Mohammad Khorasani'
