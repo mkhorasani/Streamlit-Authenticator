335ed899363a81bc8924d1cabe662285377a4df8
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2024-10-14 14:53:37 +0300

    Update two_factor_auth_model.py

--- streamlit_authenticator/models/two_factor_auth_model.py
+++ streamlit_authenticator/models/two_factor_auth_model.py
@@ -3,7 +3,7 @@ import requests
 import streamlit as st
 
 from .. import params
-from ..utilities import TwoFactorAuthError
+from ..utilities import Helpers, TwoFactorAuthError
 
 class TwoFactorAuthModel:
     """
@@ -20,6 +20,16 @@ class TwoFactorAuthModel:
         """
         self.API_key = API_key
         self.server_url = self.get_remote_variable(params['REMOTE_VARIABLES_LINK'], 'TWO_FACTOR_AUTH_SERVER_ADDRESS')
+    def generate_two_factor_auth_code(self) -> str:
+        """
+        Generates a random four digit code.
+
+        Returns
+        -------
+        str
+            Random four digit code.
+        """
+        return Helpers.generate_random_string(length=4, letters=False, punctuation=False)
     @st.cache_data(show_spinner=False)
     def get_remote_variable(self, url: str=None, variable_name: str=None) -> str:
         """
@@ -39,4 +49,26 @@ class TwoFactorAuthModel:
                 exec(content)
                 return locals()[variable_name]
         except Exception as e:
-            raise CloudError(e)
+            raise TwoFactorAuthError(e)
+    def send_email(self, recepient: str='', subject: str='', body: str='') -> bool:
+        """
+        Sends an email to a specified recepient.
+
+        Parameters
+        ----------
+        recepient: str
+            Recepient's email address.
+        subject: str
+            Email subject.
+        body: str
+            Email body.
+
+        Returns
+        -------
+        bool
+            Status of sending email, 
+            None: no email sent, 
+            True: email sent successfully, 
+            False: email failed to sent.
+        """
+        pass
