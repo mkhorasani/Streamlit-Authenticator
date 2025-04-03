39c06ee7e65a90898e5c8e2c842b0261bdec9a5a
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2025-04-04 01:26:20 +0300

    pytest

---
+++ tests/pytest.py
@@ -0,0 +1,16 @@
+"""
+Script description: This script imports tests the Streamlit-Authenticator package. 
+
+Libraries imported:
+-------------------
+- streamlit: Framework used to build pure Python web applications.
+"""
+
+from streamlit.testing.v1 import AppTest
+
+def test_login():
+    at = AppTest.from_file('tests/streamlit_authenticator_test.py').run()
+    at.text_input[0].input('jsmith').run()
+    at.text_input[1].input('abc').run()
+    at.button[0].click().run()
+    assert 'jsmith' in at.session_state['username']
